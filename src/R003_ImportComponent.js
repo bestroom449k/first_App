0fbf5bfd0d9bacd7910f90c1f8deb4227b36b34d
louie <bestroom449@gmail.com>
2025-03-31 16:25:04 +0900

    chapter 3

---
+++ src/R003_ImportComponent.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class R003_Component extends Component{
+    render() {
+        return (
+            <h2>[THIS IS A IMPORTED COMPONENT]</h2>
+        )
+    }
+}
+
+export default R003_Component;
+//React 라이브러리를 불러옵니다.
+// Component: React에서 제공하는 Component 클래스를 가져오고 class R003_Component extends Component는  클래스 이름은 R003_Component로 정하고
+// compoent해서 냐보내라 {comonent는 코드 뭉치} 
