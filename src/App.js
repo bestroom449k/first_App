6a0185260bcbb89ae486b6dd59e8f66595317b45
louie <bestroom449@example.com>
2025-03-31 17:00:23 +0900

    chapter 5

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css'; 
-import LifecycleEx from'./R004_LifecycleEx'
+import LifecycleEx from'./R005_LifecycleEx'
 
 function App () {
   return (
@@ -13,6 +13,4 @@ function App () {
 }
 
 export default App;
-// R004_LifecycleEx파일을 불러와 LifecycleEx로 장하고 이것을 내보낸다.
-// 나머지는 chapter 3랑 비슷함 
-//@testing-library/react를 사용하여 React 컴포넌트를 테스트할 수 있지만 필 수는 아니기 때문에 지움
+// R005_LifecycleEx파일을 불러와 LifecycleEx로 장하고 이것을 내보낸다.
