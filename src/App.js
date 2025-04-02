57a432fbdd9a8017a548428c22a8e32f50aca147
louie <bestroom449@example.com>
2025-04-02 15:06:31 +0900

    chapter 6

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import './App.css'; 
-import LifecycleEx from'./R005_LifecycleEx'
+import LifecycleEx from './R006_LifecycleEx'; 
 
-function App () {
+function App() {
   return (
     <div>
-      <h1>Start React 200! </h1>
-      <p>HTML 적용하기 </p>
-      <LifecycleEx></LifecycleEx>
+      <h1>Start React 200!</h1>
+      <p>HTML 적용하기</p>
+      <LifecycleEx 
+      prop_value='FromApp.js' /> 
     </div>
-  )
+  );
 }
 
 export default App;
-// R005_LifecycleEx파일을 불러와 LifecycleEx로 장하고 이것을 내보낸다.
+
+// R006_LifecycleEx파일을 불러와 LifecycleEx로 장하고 이것을 내보낸다.
+//<LifecycleEx prop_value=FromApp.js />는 LifecycleEx 컴포넌트를 사용하고 prop_value 값을 전달합니다.
+//prop_value='FromApp.js'  R006_LifecycleEx 컴포넌트에서 이 값을 받아서 사용할 수 있습니다.
+
