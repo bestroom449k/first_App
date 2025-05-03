4da3562b18540ffa432b92036bd6ec9e0470014d
louie <bestroom449@gmail.com>
2025-05-03 12:54:21 +0900

    chapter 10

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import './App.css';
-import Es6 from './R009_Es6'
+import Variable from './R010_Variable'
 
 function App() {
   return (
     <div>
       <h1>Start React 200!</h1>
       <p>CSS 적용하기</p>
-      <Es6/>
+      <Variable/>
     </div>
   );
 }
 
 export default App;
-//갔음 그리고 그냥 Es6 불러옴
