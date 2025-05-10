93cb22d3624e8632cee83c6f357932a9aae97504
louie <bestroom449@example.com>
2025-05-10 15:31:38 +0900

    chapter 20

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import React from 'react';
 import './App.css';
-import PropsDatatype from './R018_PropsDatatype'
+import PropsBoolean from './R019_PropsBoolean'
 
 function App() {
   return (
     <div>
       <h1>Start React 200!</h1>
       <p>CSS 적용하기</p>
-      <PropsDatatype 
-        String="react"
-        Number={200}
-        Boolean={1==1}
-        Array={[0, 1, 8]}
-        ObjectJson={{react:"리액트", twohundred:"200"}}
-        Function={console.log("FunctionProps: function!")}
-      />
+      <PropsBoolean BooleanTrueFalse={false}/>
+      <PropsBoolean BooleanTrueFalse/>
     </div>
-  );
-} //props에 들에 값을 집어 넣고 
+  ); // <PropsBoolean BooleanTrueFalse={false}/>이것은 거짓이면 거짓으로 올리는 것이다 그리고 만약 <PropsBoolean BooleanTrueFalse/> 이게 없으면 진실이 안 뜸
+}
 
 export default App;
