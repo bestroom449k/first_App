d7cb1e7607a70ea1ad20c781adfa7e9f6a8f41e3
louie <bestroom449@gmail.com>
2025-05-17 15:41:28 +0900

    chaper 27

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import './App.css';
-import ForceUpdate from './R026_ForceUpdate'
+import ComponentClass from './R027_ComponentClass'
 
 function App() {
   return (
     <div>
       <h1>Start React 200!</h1>
       <p>CSS 적용하기</p>
-      <ForceUpdate/>
+      <ComponentClass/>
     </div>
   );
-} //<ForceUpdate이것이SetState와 다른 저은 setState() 없이도 React 컴포넌트를 강제로 리렌더링 함
+} //클래스형 컴포넌트를 만들고 불러와서 화면에 출력
 
 export default App;
