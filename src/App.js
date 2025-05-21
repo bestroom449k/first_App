3e06e28894f73cdecbd0feee6d0ae5562d0230f9
louie <bestroom449@gmail.com>
2025-05-21 15:27:03 +0900

    chapter 32

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import './App.css';
-import ReactHook from './R031_ReactHook'
+import Fragments from './R032_Fragments'
 
 function App() {
   return (
     <div>
       <h1>Start React 200!</h1>
       <p>CSS 적용하기</p>
-      <ReactHook/>
+      <Fragments/>
     </div>
   );
-}// <ReactHook/> hook 함수는 useState, useEffect 기능이 있음 
+}  // compont 단워로 element를 할때<html>로 전채를 감싸지 않으면 에러가 생긴다고 한다. 그떄 <Fragents>로 감싸면 그렇 필요가 없어 진다.
+  // 여기서 말하는 element는 태그로 감싸는 것을 만한다. 
 
 export default App;
---
+++ src/R032_Fragments.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class R032_Fragments extends Component {
+  render() {
+    return (
+      <>
+        <p>P TAG</p>
+        <span>SPAN TAG</span>
+      </>
+    )
+  }
+}   //여기서는 Fragments를 사용해 보이는 것 처럼 <>만으로 html의 태그<<div> 없이 사용 했다.
+
+export default R032_Fragments;
