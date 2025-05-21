cbef4b47d51d6889479f6d225dba3e184fe6c39b
louie <bestroom449@gmail.com>
2025-05-21 14:26:36 +0900

    chapter 30

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import './App.css';
-import PureComponentClass from './R028_PureComponentClass'
+import FunctionComponent from './R030_FunctionComponent'
 
 function App() {
   return (
     <div>
       <h1>Start React 200!</h1>
       <p>CSS 적용하기</p>
-      <PureComponentClass/>
+      <FunctionComponent contents="[THIS IS FuntionCompoent]"/>
     </div>
   );
-} //PureComponent는 state나 props가 바뀌었을 때만 render()를 실행 
-//일반 Component 차이점	PureComponent
-//setState() 호출 시	Component 무조건 render() PureComponent 다시 실행	값이 같으면 render() 안 함
+} //FunctionComponent에 contents값을 props로 보낸다. content에는THIS IS FuntionCompoent가 들어 잇음
 
 export default App;
---
+++ src/R030_FunctionComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function R030_FunctionComponent(props){
+  let {contents}=props;
+  //contents을 받음
+  return (
+    <h2>{contents}</h2>
+  )
+}
+
+export default R030_FunctionComponent;
