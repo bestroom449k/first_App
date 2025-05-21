3fd3b120b172dc752982b655073ec1d59280d1b1
louie <bestroom449@example.com>
2025-05-21 15:00:15 +0900

    chapter 31

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import './App.css';
-import FunctionComponent from './R030_FunctionComponent'
+import ReactHook from './R031_ReactHook'
 
 function App() {
   return (
     <div>
       <h1>Start React 200!</h1>
       <p>CSS 적용하기</p>
-      <FunctionComponent contents="[THIS IS FuntionCompoent]"/>
+      <ReactHook/>
     </div>
   );
-} //FunctionComponent에 contents값을 props로 보낸다. content에는THIS IS FuntionCompoent가 들어 잇음
+}// <ReactHook/> hook 함수는 useState, useEffect 기능이 있음 
 
 export default App;
