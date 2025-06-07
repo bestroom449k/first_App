cfcbf6312eb69eb00bc94499d5f6f72bdf1a07c0
louie <bestroom449@gmail.com>
2025-06-07 17:30:09 +0900

    chaoter 59

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import React from 'react';
-import Sweetalert2Basic from './R056_Sweetalert2Basic'
-import Sweetalert2Position from './R057_Sweetalert2Position'
-import Sweetalert2Confirm from './R058_Sweetalert2Confirm'
+import FetchGet from './R059_FetchGet'
 
 function App() {
   return (
     <div>
       <h1>Start React 200!</h1>
-      <Sweetalert2Basic/>
-      <Sweetalert2Position/>
-      <Sweetalert2Confirm/>
-
+      <FetchGet/>
     </div>
   );
 }
