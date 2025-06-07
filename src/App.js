e626c51da466b586da24ff3d57ac3ae14349c1f5
louie <bestroom449@gmail.com>
2025-06-07 17:58:25 +0900

    chapter 60

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import FetchGet from './R059_FetchGet'
+import FetchPost from './R060_FetchPost'
 
 function App() {
   return (
     <div>
       <h1>Start React 200!</h1>
-      <FetchGet/>
+      <FetchPost/>
     </div>
   );
 }
 
 export default App;
+// 링크는 지금 오래되어서 작동을 하지 않음
