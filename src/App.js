f54c69fc64ce9754886cf6c4c7a8fdee34989b26
louie <bestroom449k@example.com>
2025-06-25 15:44:56 +0900

    chapter 61(다시 올리는 것)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import FetchPost from './R060_FetchPost'
+import AxiosGet from './R061_AxiosGet'
 
 function App() {
   return (
     <div>
       <h1>Start React 200!</h1>
-      <FetchPost/>
+      <AxiosGet/>
     </div>
   );
 }
 
 export default App;
-// 링크는 지금 오래되어서 작동을 하지 않음
