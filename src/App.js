f688d42f28e5d23ea4899339b6ecccabe39d6ce1
louie <bestroom449@gmail.com>
2025-08-09 16:48:57 +0900

    chapter68

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
-import R067_onChange from "./R067_onChange";
+import ReactMouseMove from "./R068_onMouseMove";
 
 function App() {
     return (
         <div>
           <h1>Start React 200!</h1>
-          <R067_onChange/>
+          <ReactMouseMove/>
         </div>
     );
 }
