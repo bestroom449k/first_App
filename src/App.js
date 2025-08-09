44f62881f9024e69cc26c4712e8ff1b88713e4fe
louie <bestroom449@gmail.com>
2025-08-09 16:08:06 +0900

    chapter 66

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import Promise from './R065_Promise'
+import React from "react";
+import R066_onclick from './R066_onclick';
 
 function App() {
-  return (
-    <div>
-      <h1>Start React 200!</h1>
-      <Promise/>
-    </div>
-  );
+    return (
+        <div>
+            <h1>Start React 200!</h1>
+            <R066_onclick />
+        </div>
+    );
 }
 
 export default App;
