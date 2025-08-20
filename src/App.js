8caae94bd2a97a23c571e3214b246ec74f834d0c
louie <bestroom449k@example.com>
2025-08-20 15:32:11 +0900

    chapter 71

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
-import ReactMouseOut from "./R070_onMouseOut";
+import R071_OnKey from "./R071_OnKey";
 
 function App() {
     return (
         <div>
           <h1>Start React 200!</h1>
-          <ReactMouseOut/>
+          <R071_OnKey/>
         </div>
     );
 }
